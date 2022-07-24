fd4c1ec919b9956496f410a016e6a87ebe0359d7
Mayk Brito <maykbrito@gmail.com>
2022-07-24 14:51:44 -0300

    trying to fix env variables in vercel

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	vite: {
+		define: {
+			'process.env': process.env,
+		},
+	},
 };
 
 export default config;
